ffa94d32ed2602faddfed85e0a709f8a01d24ff5
Vander <vander@Vanders-MacBook-Pro.local>
2023-06-29 10:15:38 -0700

    created skeleton for Angular website

--- Protract-landing-page/src/app/app.module.ts
+++ Protract-landing-page/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DemoComponent } from './demo/demo.component';
+import { FeaturesComponent } from './features/features.component';
+import { ProcessComponent } from './process/process.component';
+import { TeamComponent } from './team/team.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DemoComponent,
+    FeaturesComponent,
+    ProcessComponent,
+    TeamComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
